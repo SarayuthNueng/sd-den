74a8c9f801753cca56f79fe86d34bd5a0c943f1c
SarayuthNueng <sarayuth.navasri@gmail.com>
2022-01-17 07:52:42 +0700

    add header and sidebar in register

--- header.php
+++ header.php
@@ -23,6 +23,8 @@
     <link rel="stylesheet" href="assets/plugins/fullcalendar/fullcalendar.min.css">
     <link rel="stylesheet" href="assets/plugins/datatables/datatables.min.css">
     <link rel="stylesheet" type="text/css" href="assets/css/bootstrap-datetimepicker.min.css">
+    <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+    
     
 
   </head>
