b0a92df1e80d6acf1e2d3071811d4911fce8134f
saranueng <sarayuth.navasri@gmail.com>
2022-01-18 15:13:23 +0700

    updat sidebar colortype

--- sidebar.php
+++ sidebar.php
@@ -32,17 +32,23 @@
               </li>
               
               <li class="list-divider"></li>
-              <li class="menu-title mt-3"><span>สีแทนประเภทการนัด</span></li>
               <li class="submenu">
                 <a href="#">
-                  <i class="fas fa-laptop"></i> <span> Components </span>
-                  <span class="menu-arrow"></span
-                ></a>
+                  <i class="fas fa-tooth"></i> <span> สีแทนประเภทการนัด </span>
+                  <span class="menu-arrow"></span>
+                </a>
                 <ul class="submenu_class" style="display: none">
-                  <li><a href="uikit.php">UI Kit </a></li>
+                  <li><a style="pointer-events: none; color: #05eeff;" href="">ขูดหินปูน/อุดคอฟัน </a></li>
+                  <li><a style="pointer-events: none; color: #982abc;" href="">รักษารากฟัน/เดือยฟัน/ครอบฟัน/สะพานฟัน </a></li>
+                  <li><a style="pointer-events: none; color: #a28415;" href="">อุดฟัน </a></li>
+                  <li><a style="pointer-events: none; color: #008040;" href="">ผ่าฟันคุด/ฟันฝัง/แต่งกระดูก </a></li>
+                  <li><a style="pointer-events: none; color: #0061fe;" href="">ฟันปลอม </a></li>
+                  <li><a style="pointer-events: none; color: #f6c1d9;" href="">เด็ก </a></li>
+                  <li><a style="pointer-events: none; color: #ff0000;" href="">Swab </a></li>
+                  <li><a style="pointer-events: none; color: #000000;" href="">อื่นๆ </a></li>
                 </ul>
               </li>
-              <li class="list-divider"></li>
+              
               
             </ul>
           </div>
