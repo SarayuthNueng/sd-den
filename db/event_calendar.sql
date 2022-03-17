a04209c61acff9fd151d5156dd7048206b974571
saranueng <sarayuth.navasri@gmail.com>
2022-03-17 11:46:18 +0700

    add event-db

---
+++ db/event_calendar.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 17, 2022 at 03:20 AM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `event`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_event`
+--
+
+CREATE TABLE `event` (
+  `event_id` int(11) NOT NULL,
+  `event_title` varchar(256) NOT NULL,
+  `event_detail` text NOT NULL,
+  `event_startdate` date NOT NULL,
+  `event_enddate` date NOT NULL,
+  `event_starttime` time NOT NULL,
+  `event_endtime` time NOT NULL,
+  `event_color` varchar(15) NOT NULL DEFAULT '#FFFFFF',
+  `event_bgcolor` varchar(15) NOT NULL DEFAULT '#03a9f4',
+  `event_url` varchar(300) NOT NULL,
+  `event_repeatday` varchar(20) NOT NULL,
+  `event_allday` tinyint(1) NOT NULL,
+  `event_createdate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbl_event`
+--
+ALTER TABLE `event`
+  ADD PRIMARY KEY (`event_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbl_event`
+--
+ALTER TABLE `event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
