3aa5c92005f5b9f9cc85c0221077921feadc064e
saranueng <sarayuth.navasri@gmail.com>
2022-03-29 11:01:28 +0700

    test calendar

--- test-calendar.php
+++ test-calendar.php
@@ -1,26 +1,16 @@
-<?php
-$fullcalendar_path = "fullcalendar-4.4.2/packages/";
-?>
+
 <!DOCTYPE html>
 <html lang='en'>
 
 <head>
   <meta charset='utf-8' />
+  <link rel="stylesheet" href="fullcalendar-3.6.2/fullcalendar.min.css">
 
-  <link href='<?= $fullcalendar_path ?>/core/main.css' rel='stylesheet' />
-  <link href='<?= $fullcalendar_path ?>/daygrid/main.css' rel='stylesheet' />
-  <!--   ส่วนที่เพิ่มเข้ามาใหม่-->
-  <link href='<?= $fullcalendar_path ?>/timegrid/main.css' rel='stylesheet' />
-  <link href='<?= $fullcalendar_path ?>/list/main.css' rel='stylesheet' />
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 
-  <script src='<?= $fullcalendar_path ?>/core/main.js'></script>
-  <script src='<?= $fullcalendar_path ?>/daygrid/main.js'></script>
-  <!--   ส่วนที่เพิ่มเข้ามาใหม่-->
-  <script src='<?= $fullcalendar_path ?>/core/locales/th.js'></script>
-  <script src='<?= $fullcalendar_path ?>/timegrid/main.js'></script>
-  <script src='<?= $fullcalendar_path ?>/interaction/main.js'></script>
-  <script src='<?= $fullcalendar_path ?>/list/main.js'></script>
-
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap-theme.min.css">
 
   <style type="text/css">
     #calendar {
@@ -34,9 +24,7 @@ $fullcalendar_path = "fullcalendar-4.4.2/packages/";
 <?php include "components/header.php" ?>
 
 <?php include "components/sidebar-user-level.php" ?>
-<?php
-$fullcalendar_path = "fullcalendar-4.4.2/packages/";
-?>
+
 <div class="main-wrapper">
 		
 		<div class="page-wrapper mt-5">
@@ -45,81 +33,82 @@ $fullcalendar_path = "fullcalendar-4.4.2/packages/";
 					<div class="card">
 						<div class="card-body">
 
-              <div id='calendar'></div>
-                  <div class="modal fade" id="calendarmodal" tabindex="-1" role="dialog">
-                    <!--กำหนด id ให้กับ modal-->
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                      <div class="modal-content">
-                        <div class="modal-header">
-                          <h5 class="modal-title" id="calendarmodal-title">Modal title</h5>
-                          <!--กำหนด id ให้ส่วนหัวข้อ-->
-                          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                            <span aria-hidden="true">&times;</span>
-                          </button>
-                        </div>
-                        <div class="modal-body" id="calendarmodal-detail">
-                          <!--ำหนด id ให้ส่วนรายละเอียด-->
-                          Modal detail
-                        </div>
-                        <div class="modal-footer">
-                          <button type="button" class="btn btn-secondary" data-dismiss="modal">ปิด</button>
-                        </div>
-                      </div>
+           
+                <div class="col-md-12">
+                  <h4>Fullcalendar</h4>
+                  <!-- Button trigger modal New data-->
+                  <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#new_calendar_modal">
+                    เพิ่มข้อมูล
+                  </button>
+                  
+                  <div id='calendar'></div>
+                </div>
+              
+
+            <!-- Button trigger modal Edit data-->
+			      <span id="trigger_modal" data-toggle="modal" data-target="#calendar_modal"></span>
+
+            <!-- Modal For edit data-->
+            <div class="modal fade" id="calendar_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="text-center modal-title" id="myModalLabel">Fullcalendar</h4>
+                </div>
+                  <div id="get_calendar"></div>
+              </div>
+              </div>
+            </div>
+
+                <!-- Modal For new data-->
+            <div class="modal fade" id="new_calendar_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+              <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                <h4 class="text-center modal-title" id="myModalLabel">New Fullcalendar</h4>
+                </div>
+                <div class="modal-body">
+                  <form id="new_calendar">
+                    <div class="form-group">
+                    <label >เรื่อง</label>
+                    <input type="text" class="form-control" name="title" placeholder="">
                     </div>
-                  </div>
+                    <div class="form-group">
+                    <label >รายละเอียด</label>
+                    <input type="text" class="form-control" name="detail" placeholder="">
+                    </div>
+                    <div class="form-group">
+                    <label >วันที่เริมต้น</label>
+                    <input type="date" class="form-control" name="start"  placeholder="">
+                    </div>
+                    <div class="form-group">
+                    <label >วันที่สิ้นสุด</label>
+                    <input type="date" class="form-control" name="end"  placeholder="">
+                    </div>
+                    <input type="hidden" name="new_calendar_form">
+                  </form>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-primary" onclick="return new_calendar();">บันทึกข้อมูล</button>
+                  <button type="button" class="btn btn-default" data-dismiss="modal">ปิด</button>
+                
+                </div>
+              </div>
+              </div>
+            </div>
 
-                  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha256-4+XzXVhsDmqanXGHaHvgh1gMQKX40OUvDEBTu8JcmNs=" crossorigin="anonymous"></script>
-                  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.bundle.min.js"></script>
+            <!-- Javascript -->
+            <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script> 
+            <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+            <script type="text/javascript" src="fullcalendar-3.6.2/lib/moment.min.js"></script>
+            <script type="text/javascript" src="fullcalendar-3.6.2/fullcalendar.min.js"></script>
+            <script type="text/javascript" src="fullcalendar-3.6.2/locale/th.js"></script>
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 
-                  <script type="text/javascript">
-                    var calendar; // สร้างตัวแปรไว้ด้านนอก เพื่อให้สามารถอ้างอิงแบบ global ได้
-                    $(function() {
-                      // กำหนด element ที่จะแสดงปฏิทิน
-                      var calendarEl = $("#calendar")[0];
-
-                      // กำหนดการตั้งค่า
-                      calendar = new FullCalendar.Calendar(calendarEl, {
-                        plugins: ['interaction', 'dayGrid', 'timeGrid', 'list'], // plugin ที่เราจะใช้งาน
-                        defaultView: 'dayGridMonth', // ค้าเริ่มร้นเมื่อโหลดแสดงปฏิทิน
-                        header: {
-                          left: 'prev,next today',
-                          center: 'title',
-                          right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-                        },
-                        events: { // เรียกใช้งาน event จาก json ไฟล์ ที่สร้างด้วย php
-                          url: 'events.php?gData=1',
-                          error: function() {
-
-                          }
-                        },
-                        eventLimit: true, // allow "more" link when too many events
-                        locale: 'th', // กำหนดให้แสดงภาษาไทย
-                        firstDay: 0, // กำหนดวันแรกในปฏิทินเป็นวันอาทิตย์ 0 เป็นวันจันทร์ 1
-                        showNonCurrentDates: false, // แสดงที่ของเดือนอื่นหรือไม่
-                        eventTimeFormat: { // รูปแบบการแสดงของเวลา เช่น '14:30' 
-                          hour: '2-digit',
-                          minute: '2-digit',
-                          meridiem: false
-                        }
-                      });
-
-                      // แสดงปฏิทิน 
-                      calendar.render();
-
-                    });
-                  </script>
-
-                  <script type="text/javascript">
-                    function viewdetail(id) {
-                      // ก่อนที่ modal จะแสดง
-                      $('#calendarmodal').on('show.bs.modal', function(e) {
-                        var event = calendar.getEventById(id) // ดึงข้อมูล ผ่าน api
-                        $("#calendarmodal-title").html(event.title);
-                        $("#calendarmodal-detail").html(event.extendedProps.detail); // ข้อมูลเพิ่มเติมจะเรียกผ่าน extendedProps
-                      });
-                      $("#calendarmodal").modal(); // แสดง modal
-                    }
-                  </script>
+            <!-- นำเข้า script File -->
+            <script src='script.js'></script>	
+                  
 
 						</div>
 					</div>
@@ -128,7 +117,6 @@ $fullcalendar_path = "fullcalendar-4.4.2/packages/";
 		</div>
 
 </div>
-	<script src="components/assets/js/jquery-3.5.1.min.js"></script>
 	<script src="components/assets/js/popper.min.js"></script>
 	<script src="components/assets/js/bootstrap.min.js"></script>
 	<script src="components/assets/plugins/slimscroll/jquery.slimscroll.min.js"></script>
